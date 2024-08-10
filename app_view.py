4f1f59c6c924522d12873339713d84068404213d
Zediious95 <177992520+Zediious95@users.noreply.github.com>
2024-08-09 22:49:31 -0400

    Update app_view.py

--- app_view.py
+++ app_view.py
@@ -30,7 +30,7 @@ class AppView:
             st.markdown("**Disclaimer:** Use at your own risk.")
 
         with st.sidebar:
-            st.markdown("Easily create and import flashcards directly into Anki with PDF-Anki -- powered by GPT3.5-turbo from OpenAI.\n Alternate link: [pdftoanki.xyz](https://pdftoanki.xyz)")
+            st.markdown("Easily create and import flashcards directly into Anki with PDF-Anki -- powered by GPT 4o mini from OpenAI.\n Alternate link: [pdftoanki.xyz](https://pdftoanki.xyz)")
             badge(type="twitter", name="PDFToAnki")
             api_key = st.empty()
             api_key_text = st.empty()
@@ -49,7 +49,7 @@ class AppView:
                 # if "fine_tuning" in st.session_state and st.session_state["fine_tuning"] == True:
                 #     st.session_state["model"] = "ft:gpt-3.5-turbo-0125:personal:pdf-anki-new:9O0JdsS2"
                 # else:
-                st.session_state["model"] = "gpt-3.5-turbo"
+                st.session_state["model"] = "gpt-4o-mini"
 
                 api_key.empty()
                 api_key_text.empty()
