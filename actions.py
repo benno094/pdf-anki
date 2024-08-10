4d4cc3db73bdbce8962357519855220f88eddb15
Zediious95 <177992520+Zediious95@users.noreply.github.com>
2024-08-10 15:43:14 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -3,6 +3,8 @@
 import base64
 import json
 import os
+import fitz  # PyMuPDF
+from PIL import Image
 from io import BytesIO
 import openai
 from openai import OpenAI
@@ -10,6 +12,7 @@ import re
 import streamlit as st
 import streamlit.components.v1 as components
 import markdown
+
 client = OpenAI()
 
 # Custom component to call AnkiConnect on client side
@@ -17,9 +20,12 @@ parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "API/frontend/build")
 _API = components.declare_component("API", path=build_dir)
 
-def API(action, key=None, deck=None, image = None, front=None, back=None, tags=None, flashcards = None):
-    component_value = _API(action=action, key=key, deck=deck, image = image, front=front, back=back, tags=tags, flashcards=flashcards)
+
+def API(action, key=None, deck=None, image=None, front=None, back=None, tags=None, flashcards=None):
+    component_value = _API(action=action, key=key, deck=deck, image=image, front=front, back=back, tags=tags,
+                           flashcards=flashcards)
     return component_value
+
 
 class Actions:
     def __init__(self, root):
@@ -45,11 +51,11 @@ class Actions:
 
         try:
             completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"Return in one word the language of this text: {text}"}
-            ]
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": f"Return in one word the language of this text: {text}"}
+                ]
             )
         except openai.APIError as e:
             st.warning(f"OpenAI API returned an API Error:\n\n{str(e)}\n\n**Refresh the page and try again**")
@@ -60,46 +66,30 @@ class Actions:
             st.session_state["openai_error"] = e
             st.stop()
         except openai.RateLimitError as e:
-            st.warning(f"OpenAI API request exceeded rate limit::\n\n{str(e)}\n\n**Fix the problem, refresh the page and try again**")
+            st.warning(
+                f"OpenAI API request exceeded rate limit::\n\n{str(e)}\n\n**Fix the problem, refresh the page and try again**")
             st.session_state["openai_error"] = e
             st.stop()
 
         return completion.choices[0].message.content
 
-    def send_to_gpt(self, page, image = None):
+    def send_to_gpt(self, page):
         # TODO: Check token count and send several pages, if possible
         # TODO: Add timeout
         # if "prompt" not in st.session_state:
-            # if st.session_state["fine_tuning"] == True:
+        # if st.session_state["fine_tuning"] == True:
         # st.session_state["prompt"] = "You are receiving the text from one slide of a lecture. Use the following principles when making the flashcards. Return json."
-            # else:
+        # else:
         st.session_state["prompt"] = """
-You are receiving the text from one slide of a lecture. Use the following principles when making the Anki cloze deletion cards.:
+You are receiving the text from one slide of a lecture. Use the following principles when making the flashcards:
 
-Task: Your task is to thoroughly analyze the source material and condense the information into concise and direct statements using Anki cloze deletion mark-up. Ensure that each statement is clearly written, easily understandable, and adheres to the specified formatting and reference criteria. 
-
-Formatting Criteria: 
-- Construct a table with two columns: "statement" and "explanation".
-- Each row of the "statement" column should contain a single statement written in Anki cloze deletion mark-up, focusing on the source material.
-- Each row of the "explanation" column should provide additional explanations for the corresponding "statement". There should be no cloze deletions in this column.
-
-Reference Criteria for each "statement":
-- Restrict each statement to 2 cloze deletions. If needed, you may add 1-2 more cloze deletions but restrict them to either cloze1 or cloze2.
-- Limit the word count of each statement to less than 40 words.
-- Keep the text within the cloze deletions limited to one or two Source key words.
-- Each statement must be able to stand alone. Include the subject of the statement somewhere in the text.
-- Keep ONLY simple, direct, cloze deletion statements in the "Statements" column. Keep any additional explanatory information in the "explanation" column search the, and research USMLE textbook for detailed explanations supporting the statement.
-
-Example: 
-| Front | Back |
-| --- | --- |
-| {{c1::word}} Give an example of what you want here | - Example of notes here |
-| This is a {{c1::second}} example to reinforce the formatting. | - More example notes here |
-| The {{c1::olfactory nerve}} is responsible for the sense of {{c2::smell}} | - It is the 1st cranial nerve. It's the only cranial nerve that isn't myelinated |
-| {{c1::Chromosomes}} are organized structures of DNA and proteins found in the {{c2::nucleus}} | - They contain many genes, regulatory elements, and intervening sequences |
-| DNA {{c1::methylation}} at {{c2::CpG}} islands {{c3::represses}} transcription | - Methylation is mostly relevant to key developmental genes - Mechanistically, methylation is used to disrupt binding of transcription factors, recruit "readers", recruit heterochromatin protein |
-| Hypoxia may be caused by {{c1::carbon monoxide}} poisoning, which decreases the O2 content of blood (CaO2) | - Cyanide poisoning, on the other hand, increases O2 content as it's not getting extracted (cyanide binds a3 in Fe3+ state in complex IV) |
-- Statement and explanation must be in """ + st.session_state["lang"] + """.
+- Create Anki flashcards for an exam at university level.
+- Each question is standalone.
+- Short answer.
+- All information on slide needs to be used and only use the information that is on the slide.
+- Answers should be on the back and not included in the question.
+- Only add each piece of information once.
+- Questions and answers must be in """ + st.session_state["lang"] + """.
 - Ignore information about the uni, course, professor or auxiliary slide information.
 - If whole slide fits on one flashcard, do that.
 - Use 'null_function' if page is just a table of contents, learning objectives or a title slide.
@@ -136,7 +126,7 @@ Example:
                 # )
                 completion = client.chat.completions.create(
                     model=st.session_state["model"],
-                    response_format={ "type": "json_object" },
+                    response_format={"type": "json_object"},
                     messages=[
                         {
                             "role": "system",
@@ -158,13 +148,15 @@ Example:
                                     "properties": {
                                         "flashcards": {
                                             "type": "array",
-                                                "items": {
-                                                    "type": "object",
-                                                    "properties": {
-                                                    "front": {"type": "string", "description": "Front side of the flashcard; statement"},
-                                                    "back": {"type": "string", "description": "Back side of the flashcard; explanation"}
-                                                    }
+                                            "items": {
+                                                "type": "object",
+                                                "properties": {
+                                                    "front": {"type": "string",
+                                                              "description": "Front side of the flashcard; a question"},
+                                                    "back": {"type": "string",
+                                                             "description": "Back side of the flashcard; the answer"}
                                                 }
+                                            }
                                         }
                                     }
                                 }
@@ -190,14 +182,16 @@ Example:
             except openai.APIConnectionError as e:
                 continue
             except openai.RateLimitError as e:
-                    st.warning(f"OpenAI API request exceeded rate limit::\n\n{str(e)}\n\n**Fix the problem, refresh the page and try again**")
-                    st.session_state["openai_error"] = e
-                    st.stop()
-            
+                st.warning(
+                    f"OpenAI API request exceeded rate limit::\n\n{str(e)}\n\n**Fix the problem, refresh the page and try again**")
+                st.session_state["openai_error"] = e
+                st.stop()
+
             print(f"Call no. {str(retries + 1)} for slide {str(page + 1)}")
             if completion.choices[0].message.tool_calls is not None:
 
-                if completion.choices[0].message.tool_calls[0].function.name == "null_function" or completion.choices[0].message.content == "null_function":
+                if completion.choices[0].message.tool_calls[0].function.name == "null_function" or completion.choices[
+                    0].message.content == "null_function":
                     st.session_state[f"{str(page)}_is_title"] = True
                     return None
 
@@ -206,7 +200,7 @@ Example:
                     return completion.choices[0].message.tool_calls[0].function.arguments
                 elif completion.choices[0].message.content:
                     return completion.choices[0].message.content
-    
+
             except Exception as e:
                 print("Error: ", e)
                 print("Returned response:\n", completion.choices[0].message.tool_calls)
@@ -233,25 +227,19 @@ Example:
                 back = markdown.markdown(card['back'], extensions=['nl2br'])
                 tags = st.session_state["flashcards_" + str(page) + "_tags"]
                 note = {
-                "deck": deck,
-                "front": front,
-                "back": back,
-                "tags": [tags]
+                    "deckName": deck,
+                    "modelName": "Cloze",
+                    "front": front,
+                    "back": back,
+                    "tags": [tags]
                 }
 
-                if f"img_{page, no}" in st.session_state:
-                    image_bytes = BytesIO()
-                    st.session_state[f"img_{page, no}"].save(image_bytes, format='JPEG')
-                    image_bytes.seek(0)
-                    image = base64.b64encode(image_bytes.getvalue())
-                    note["image"] = image
-
                 notes.append(note)
-                
-            API("addNotes", deck = deck, flashcards = notes)
+
+            API("addNotes", deck=deck, flashcards=notes)
             return True
         except Exception as e:
-            raise ValueError("add_to_anki error: ", e)
+             raise ValueError("add_to_anki error: ", e)
 
     def cleanup_response(self, text):
         try:
@@ -259,7 +247,7 @@ Example:
             prefix = 'flashcard_function('
             if text.startswith(prefix):
                 text = text[len(prefix):-1]  # remove the prefix and the closing parenthesis
-            
+
                 json_strs = text.strip().split('\n})\n')
                 json_strs = [text + '}' if not text.endswith('}') else text for text in json_strs]
                 json_strs = ['{' + text if not text.startswith('{') else text for text in json_strs]
@@ -275,12 +263,13 @@ Example:
             # print("Curly quotes removed:", response_text_standard_quotes)
 
             # Replace inner double quotes with single quotes
-            response_text_single_quotes = re.sub(r'("(?:[^"\\]|\\.)*")', self.replace_inner_double_quotes, response_text_standard_quotes)
+            response_text_single_quotes = re.sub(r'("(?:[^"\\]|\\.)*")', self.replace_inner_double_quotes,
+                                                 response_text_standard_quotes)
             # print("Double quotes removed:", response_text_single_quotes)
 
             # Parse the JSON data
             response_cards = json.loads(response_text_single_quotes, strict=False)
-            # print("Parsed:", response_cards)            
+            # print("Parsed:", response_cards)
 
             # Extract the "flashcards" array
             response_data = response_cards["flashcards"]
@@ -297,7 +286,7 @@ Example:
 
     def replace_curly_quotes(self, text):
         return text.replace('“', "'").replace('”', "'").replace('„', "'")
-    
+
     def replace_inner_double_quotes(self, match_obj):
         inner_text = match_obj.group(0)
         # Match the fields containing double quotes
@@ -310,4 +299,3 @@ Example:
             inner_text = inner_text.replace(match[1], inner_quotes_replaced)
 
         return inner_text
-          
